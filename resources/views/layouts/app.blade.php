39e3de4da429a88ad4361baa2e8e931bd226b122
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2024-03-03 13:53:46 -0600

    Update app.blade.php
    No puse los charts donde era :sob:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,12 +93,6 @@
                 </div>
             </aside>
             <main class="app-main">
-                <br>
-                <h1>Dashboard</h1>
-                <iframe 
-                    style="background: #F1F5F4;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);width: 78.5vw;height: 100vh;"  
-                    src="https://charts.mongodb.com/charts-db-project-1-qqzhs/embed/dashboards?id=65daa048-d109-47e4-877f-19f06a1bdc84&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=fixed&scalingHeight=fixed">
-                </iframe>
                 <div class="app-content-header">
                     <div class="container-fluid">
                         <div class="row">
